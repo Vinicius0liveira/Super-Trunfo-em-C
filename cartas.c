4105b7b4265b0afe822521393970a57ccbcdc056
Vinicius <silvaoliveirav227@gmail.com>
2025-08-10 19:02:07 -0300

    atualizações

--- cartas.c
+++ cartas.c
@@ -18,56 +18,7 @@ int main() {
     float Pib2 = 300000000000.50;
     int Numero_de_pontos_Turisticos2 = 30; 
 
-    char estado3[] = "C";
-    char codigo_da_carta3[] = "C03";
-    char Nome_da_Cidade3[] = "Belo Horizonte";
-    int População3 = 2527000;
-    float Area3 = 331.0;
-    float Pib3 = 100000000000.0;
-    int Numero_de_pontos_Turisticos3 = 25;
-
-    char estado4[] = "D";
-    char codigo_da_carta4[] = "D04";
-    char Nome_da_Cidade4[] = "Salvador";
-    int População4 = 2923000;
-    float Area4 = 693.0;
-    float Pib4 = 150000000000.0;
-    int Numero_de_pontos_Turisticos4 = 20;
-
-    char estado5[] = "E";
-    char codigo_da_carta5[] = "E05";
-    char Nome_da_Cidade5[] = "Fortaleza";
-    int População5 = 2687000;
-    float Area5 = 314.9;
-    float Pib5 = 100000000000.0;
-    int Numero_de_pontos_Turisticos5 = 15;
-
-    char estado6[] = "F";
-    char codigo_da_carta6[] = "F06";
-    char Nome_da_Cidade6[] = "Curitiba";
-    int População6 = 1963000;
-    float Area6 = 432.0;
-    float Pib6 = 100000000000.0;
-    int Numero_de_pontos_Turisticos6 = 10;
-
-    char estado7[] = "G";
-    char codigo_da_carta7[] = "G07";
-    char Nome_da_Cidade7[] = "Manaus";
-    int População7 = 2182000;
-    float Area7 = 1570.0;
-    float Pib7 = 50000000000.0;
-    int Numero_de_pontos_Turisticos7 = 5;
-
-    char estado8[] = "H";
-    char codigo_da_carta8[] = "H08";
-    char Nome_da_Cidade8[] = "Recife";
-    int População8 = 1646000;
-    float Area8 = 218.0;
-    float Pib8 = 100000000000.0;
-    int Numero_de_pontos_Turisticos8 = 12;
-
-
-
+    
     printf("Estado: %s\n ", estado);
     printf("Código da Carta: %s\n ", codigo_da_carta);
     printf("Nome da Cidade: %s\n", Nome_da_Cidade);
@@ -85,66 +36,6 @@ int main() {
     printf("Área: %.2f km²\n -", Area2);
     printf("PIB: R$ %.2f bilhões R$\n ", Pib2);
     printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_Turisticos2);
-
-    printf("\n\n");
-
-    printf("Estado: %s\n ", estado3);
-    printf("Código da Carta: %s\n ", codigo_da_carta3);
-    printf("Nome da Cidade: %s\n", Nome_da_Cidade3);
-    printf("População: %d\n", População3);
-    printf("Área: %.2f km²\n -", Area3);
-    printf("PIB: R$ %.2f bilhões R$\n ", Pib3);
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_Turisticos3);  
-
-    printf("\n\n");
-
-    printf("Estado: %s\n ", estado4);
-    printf("Código da Carta: %s\n ", codigo_da_carta4);
-    printf("Nome da Cidade: %s\n", Nome_da_Cidade4);
-    printf("População: %d\n", População4);
-    printf("Área: %.2f km²\n -", Area4);
-    printf("PIB: R$ %.2f bilhões R$\n ", Pib4);
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_Turisticos4);
-
-    printf("\n\n"); 
-
-    printf("Estado: %s\n ", estado5);
-    printf("Código da Carta: %s\n ", codigo_da_carta5);
-    printf("Nome da Cidade: %s\n", Nome_da_Cidade5);
-    printf("População: %d\n", População5);
-    printf("Área: %.2f km²\n -", Area5);
-    printf("PIB: R$ %.2f bilhões R$\n ", Pib5);
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_Turisticos5);
-
-    printf("\n\n");
-
-    printf("Estado: %s\n ", estado6);
-    printf("Código da Carta: %s\n ", codigo_da_carta6);
-    printf("Nome da Cidade: %s\n", Nome_da_Cidade6);
-    printf("População: %d\n", População6);
-    printf("Área: %.2f km²\n -", Area6);
-    printf("PIB: R$ %.2f bilhões R$\n ", Pib6);
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_Turisticos6);
-
-    printf("\n\n");
-
-    printf("Estado: %s\n ", estado7);
-    printf("Código da Carta: %s\n ", codigo_da_carta7);
-    printf("Nome da Cidade: %s\n", Nome_da_Cidade7);
-    printf("População: %d\n", População7);
-    printf("Área: %.2f km²\n -", Area7);
-    printf("PIB: R$ %.2f bilhões R$\n ", Pib7);
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_Turisticos7);
-
-    printf("\n\n");
-
-    printf("Estado: %s\n ", estado8);
-    printf("Código da Carta: %s\n ", codigo_da_carta8);
-    printf("Nome da Cidade: %s\n", Nome_da_Cidade8);
-    printf("População: %d\n", População8);
-    printf("Área: %.2f km²\n -", Area8);
-    printf("PIB: R$ %.2f bilhões R$\n ", Pib8);
-    printf("Número de Pontos Turísticos: %d\n", Numero_de_pontos_Turisticos8);
 
     return 0;
 
